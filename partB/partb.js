7c033677185f219c383a17a12c9e141079257358
PrarthanaNemade14 <nemade.pr@northeastern.edu>
2022-10-27 13:23:59 -0400

    Changes

--- partB/partb.js
+++ partB/partb.js
@@ -1,29 +1,29 @@
 
-
+/*
 hoursCount = 0, minutesCount = 0, secondsCount = 0;
 
-hours = document.getElementById("hours");
-minutes = document.getElementById("minutes");
-seconds = document.getElementById("seconds");
+hours = document.getElementById('hours')
+minutes = document.getElementById('minutes')
+seconds = document.getElementById('seconds')
 
 
 function strt(){
-    $(#stopCount).removeAttr('disabled');
-    $(#resetCount).removeAttr('disabled');
-    $(#startCount).attr({'disabled' : 'disabled'});
+    $("#stopCount").removeAttr('disabled')
+    $("#resetCount").removeAttr('disabled')
+    $("#startCount").attr({'disabled' : 'disabled'})
 
-    setInterval(function(){
-        hoursCount += 1;
-        hours.innerHTML = hoursCount;
+    hoursSetInterval = setInterval(function(){
+        hoursCount += 1
+        hours.innerHTML = hoursCount
     },60000);
 
-    setInterval(function(){
+    minutesSetInterval = setInterval(function(){
         minutesCount += 1
-        
+        minutes.innerHTML = minutesCount
         minutes.innerHTML = minutesCount
     },1000);
 
-    setInterval(function(){
+    secondsSetInterval =setInterval(function(){
         secondsCount += 1
         if(secondsCount > 59){
             secondsCount = 1
@@ -35,16 +35,80 @@ function strt(){
 }
 
 function stp(){
-    $(#startCount).removeAttr('disabled');
+    $("#startCount").removeAttr('disabled');
     //$(#resetcount).removeAttr('disabled');
-    $(#stopCount).attr({'disabled' : 'disabled'});
+    $("#stopCount").attr({'disabled' : 'disabled'});
+
+    clearInterval(hoursSetInterval)
+	clearInterval(minutesSetInterval)
+	clearInterval(secondsSetInterval)
 
 }
 
 function rst(){
-    $(#startCount).removeAttr('disabled');
-    $(#resetCount).attr({'disabled' : 'disabled'});
-    $(#stopCount).attr({'disabled' : 'disabled'});
+    $("#startCount").removeAttr('disabled');
+    $("#resetCount").attr({'disabled' : 'disabled'});
+    $("#stopCount").attr({'disabled' : 'disabled'});
 
-
+    clearInterval(hoursSetInterval)
+    clearInterval(minutesSetInterval)
+	clearInterval(secondSetInterval)
+	hoursCount = 0, minutesCount = 0,secondCount = 0
+    hours.innerHTML = hoursCount
+	minutes.innerHTML = minutesCount
+	seconds.innerHTML = secondsCount
 }
+}
+*/
+
+minutesCount = 0,secondCount = 0,centiSecondCount = 0
+minutes = document.getElementById('minutes')
+second = document.getElementById('second')
+centiSecond = document.getElementById('centiSecond')
+function startSW(){
+	$("#pauseCount").removeAttr("disabled")
+	$("#resetCount").removeAttr("disabled")
+	$("#startCount").attr({"disabled":"disabled"})
+	minutesSetInterval = setInterval(function(){
+		minutesCount += 1
+		minutes.innerHTML = minutesCount
+	},60000);
+	secondSetInterval = setInterval(function(){
+		secondCount += 1
+		if(secondCount > 59){
+			secondCount = 1
+		}
+		second.innerHTML = secondCount
+	},1000);
+	centiSecondSetInterval = setInterval(function(){
+		centiSecondCount += 1
+		if(centiSecondCount > 99){
+			centiSecondCount = 1
+		}
+		centiSecond.innerHTML = centiSecondCount
+	},10);
+}
+
+function pauseSW(){
+	$("#startCount").removeAttr("disabled")
+	$("#pauseCount").attr({"disabled":"disabled"})
+	clearInterval(minutesSetInterval)
+	clearInterval(secondSetInterval)
+	clearInterval(centiSecondSetInterval)
+}
+
+function restartSW(){
+	$("#pauseCount").attr({"disabled":"disabled"})
+	$("#resetCount").attr({"disabled":"disabled"})
+	$("#startCount").removeAttr("disabled")
+
+	clearInterval(minutesSetInterval)
+	clearInterval(secondSetInterval)
+	clearInterval(centiSecondSetInterval)
+	minutesCount = 0,secondCount = 0,centiSecondCount = 0
+	minutes.innerHTML = minutesCount
+	second.innerHTML = secondCount
+	centiSecond.innerHTML = centiSecondCount
+}
+
+
